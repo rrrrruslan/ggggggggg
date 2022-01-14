de4bea5aee87d66967e0404e86debb976eee7117
rrrrruslan <rus-kuznetsov-02@mail.ru>
2022-01-14 11:52:18 +0700

    jr

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter2.Task3;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter2.Task3.Program.run(args);
+        }
+    }
+}
